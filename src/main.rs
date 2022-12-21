4942e873081af3711e56505bce2fc86b8d88ae69
C M Madhav Prabhu <dpmadhav@hotmail.com>
2022-12-21 16:04:48 +0530

    Guessing Game with basic error handling
    * Learnt enum types
    * match keyword, Ok and Err. Scope of variables
    * Conversion, err cases in i32 and u32 when converting fro mstring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+//stdio library
+//
+use rand::Rng;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("Secret number is {}", secret_number);
+
+    loop {
+        println!("Guess a number!");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error in reading line");
+
+        let guess_: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                if guess == "quit" {
+                    println!("Thank you for playing");
+                    break;
+                } else {
+                    println!("Error in input, try again");
+                    continue;
+                }
+            }
+        };
+
+        match guess_.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Small"),
+            std::cmp::Ordering::Greater => println!("Big"),
+            std::cmp::Ordering::Equal => {
+                println!("Right!");
+                break;
+            }
+        }
+    }
 }
